d09433d53622fe83f4523cea8f6c271205c0f97a
musong <txz32102@gmail.com>
2023-09-04 22:32:03 +0800

    add vue

---
+++ vue-project/src/components/DataProcess.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const greeting = ref('upload file!')
+</script>
+
+<template>
+  <p class="greeting">{{ greeting }}</p>
+  <form action="/upload" method="post" enctype="multipart/form-data">
+  <label for="fileUpload">Select a file:</label>
+  <input type="file" id="fileUpload" name="fileUpload">
+  <input type="submit" value="Upload">
+</form>
+</template>
+
+<style>
+.greeting {
+  color: rgb(155, 88, 88);
+  font-weight: bold;
+}
+</style>
