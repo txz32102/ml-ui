abdc0878691eb7000b39c472f4d607bc83424686
musong <txz32102@gmail.com>
2023-09-05 16:10:59 +0800

    preview

--- frontend/src/components/DataProcess.vue
+++ frontend/src/components/DataProcess.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const greeting = ref('upload file!')
+function showPreview(event){
+  if(event.target.files.length > 0){
+    var src = URL.createObjectURL(event.target.files[0]);
+    var preview = document.getElementById("file-ip-1-preview");
+    preview.src = src;
+    preview.style.display = "block";
+  }
+}
 </script>
 
 <template>
@@ -9,7 +17,20 @@ const greeting = ref('upload file!')
   <label for="fileUpload">Select a file:</label>
   <input type="file" id="fileUpload" name="fileUpload">
   <input type="submit" value="Upload">
-</form>
+  </form>
+
+  <label for="file-ip-1">Upload Image</label>
+ <input type="file" id="file-ip-1" accept="image/*" onchange="showPreview(event);">
+  <div class="center">
+
+  <div class="form-input">
+
+    <div class="preview">
+   <img id="file-ip-1-preview">
+ </div>
+
+  </div>
+</div> 
 </template>
 
 <style>
