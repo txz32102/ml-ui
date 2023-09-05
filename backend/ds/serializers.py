a2d61e142aa63e4d0ce73c0dd090b81ed95feb8a
musong <txz32102@gmail.com>
2023-09-05 16:11:30 +0800

    preview

--- backend/ds/serializers.py
+++ backend/ds/serializers.py
@@ -1,2 +1,12 @@
 from rest_framework import serializers
-from .models import
+from .models import json_api_test
+
+class testapi(serializers.ModelSerializer):
+    tags = serializers.SerializerMethodField()
+
+    def get_tags(self, instance):
+        print("hello")
+
+    class Meta:
+        model = json_api_test
+        fields = ('keyword', 'tags')
